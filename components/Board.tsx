e9fc6d8469ed65ad7305608a539f995d17186af3
birtukan <birtukandegu2127@gmail.com>
2025-10-05 04:28:19 +0300

    feat: implement Board and Column components with card functionality and default data

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,14 +1,50 @@
 'use client'
 
 import React, { useState } from 'react'
+import Column from './Column';
+import { DEFAULT_CARDS } from '@/data';
 
 const Board = () => {
 
-  const [cards, setCards] = useState({});
+  const [cards, setCards] = useState(DEFAULT_CARDS);
 
   return (
     <div className="flex h-full w-full gap-3 overflow-scroll p-12">
-        Board
+      <Column
+        title="Draft"
+        column="draft"
+        headingColor="text-neutral-500"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="Happy"
+        column="happy"
+        headingColor="text-yellow-200"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="Calm"
+        column="calm"
+        headingColor="text-blue-200"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="Sad"
+        column="sad"
+        headingColor="text-emerald-200"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="Angry"
+        column="angry"
+        headingColor="text-red-200"
+        cards={cards}
+        setCards={setCards}
+      />      
     </div>
   )
 }
---
+++ data/index.ts
@@ -0,0 +1,26 @@
+export const DEFAULT_CARDS: CardType[] = [
+ // Draft
+  { title: "Completed routine chores like laundry and cleaning, nothing exciting", id: "7", column: "draft" },
+  { title: "Went grocery shopping and ran errands, a bit tiring but necessary", id: "8", column: "draft" },
+  { title: "Spent some time replying to emails and messages, felt a bit drained", id: "9", column: "draft" },
+
+// HAPPY
+  { title: "Went for a long walk in the park and enjoyed the fresh air", id: "1", column: "happy" },
+  { title: "Received a kind message from a friend that brightened my day", id: "2", column: "happy" },
+  { title: "Tried a new recipe for lunch and it turned out really well", id: "3", column: "happy" },
+
+  // CALM
+  { title: "Spent some quiet time reading a chapter of my favorite book", id: "4", column: "calm" },
+  { title: "Organized my desk and workspace to create a more peaceful environment", id: "5", column: "calm" },
+  { title: "Watered my plants and took a moment to enjoy the little things", id: "6", column: "calm" },
+
+
+  // SAD
+  { title: "Missed a phone call with a friend I was hoping to talk to", id: "10", column: "sad" },
+  { title: "Forgot to write in my journal today, felt a bit disappointed in myself", id: "11", column: "sad" },
+
+  // ANGRY
+  { title: "Spilled coffee on my desk while rushing this morning, frustrating start to the day", id: "12", column: "angry" },
+  { title: "Got stuck in traffic on the way home and felt tense and irritated", id: "13", column: "angry" },
+];
+
